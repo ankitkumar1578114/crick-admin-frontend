4e5a503664a3370a00a428a204b7a243e2e609b9
Ankit Kumar <ankitkumar1578114@gmail.com>
2023-07-20 13:57:19 +0530

    some chagnes

--- src/pages/Match/utlis/match-table.js
+++ src/pages/Match/utlis/match-table.js
@@ -53,10 +53,12 @@ export const columns = [
         <>
         {
             {
+              0: <Pill type="transparent" content={moment(row?.start_time).format('hh:mm DD MMM YYYY') }/>,
               '-1': <Pill content="Runnng"/>,
               1: <Pill content="Runnng"/>,
-              0: <Pill type="transparent" content={moment(row?.start_time).format('hh:mm DD MMM YYYY') }/>,
-              2: <Pill type="secondary" content = 'Finished'/>
+              '-2': <Pill content="Runnng"/>,
+              2: <Pill content="Runnng"/>,
+              3: <Pill type="secondary" content = 'Finished'/>
             }[row?.current_inning]
         }
         </>
